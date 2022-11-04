800c519a60519a385d26e49cf63ee56a1293d109
angel hernandez <angel.hernandez_santiago@telus.com>
2022-11-04 10:51:38 -0600

    modificadores en solidity

---
+++ introSolidity/modificiadores.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract modificadores { 
+
+    address private owner;
+
+    constructor () { 
+        owner = msg.sender;
+    }
+
+    function Suma (uint numero1, uint numero2) public view EsOwner() returns (uint) { 
+        return numero1 + numero2; 
+    }
+
+    modifier EsOwner () {  // Los modifiers son limitadores a las funciones de un contrato
+        if (msg.sender != owner) revert(); // si el sender NO es el "owner", se interrumpe la funcion usando el "revert". 
+        _; // ! el "_" sirve para indicar que si la condicion NO se cumple, el programa debe seguir. (el _ es lo que finaliza el modifier)
+    }
+}
